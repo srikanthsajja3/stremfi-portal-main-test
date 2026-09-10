d5cd4e866c85db4dec371d4aeaab9a92140a2eba
srikanthsajja3 <srikanthsajja360@gmail.com>
2026-09-10 21:15:47 +0530

    Configure Vercel API proxy rewrites to resolve CORS issues

--- src/config/env.js
+++ src/config/env.js
@@ -4,7 +4,7 @@
  * "https://vrplay.in" directly.
  */
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "https://vrplay.in/portal_php";
+  process.env.REACT_APP_API_BASE_URL || "/api_proxy";
 
 // Used for building <img src> URLs (e.g. https://vrplay.in/images/vrplay1/xyz.png)
 export const ASSET_BASE_URL =
