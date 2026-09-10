28cdb4560c2636736e20146fb15be33065bd485d
srikanthsajja3 <srikanthsajja360@gmail.com>
2026-09-10 21:19:56 +0530

    Restore original working API endpoints for successful login

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "/api_proxy",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "https://pol.vrplay.in/frontend_api",
 });
 
 apiClient.interceptors.request.use((config) => {
